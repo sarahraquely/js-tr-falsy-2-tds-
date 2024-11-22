b1054c394ee655a5c90857e753d132d10de112a6
sarahraquely <160151647+sarahraquely@users.noreply.github.com>
2024-11-22 12:15:00 -0300

    carga_js-tr-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,35 @@
+const x = 0;
+//Boolean
+const usuarioLogado = true
+const contaPaga = false 
+
+// truthy ou falsy
+// 0 -> false
+// 1 -> true
+
+console.log(0==false)//Retorna true
+console.log(""==false)//Retorna true
+console.log(1==true)
+
+console.log("null e undefinned")
+//undefinned 
+//null-> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log("quando o undefinned esta em sua forma pura");
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
